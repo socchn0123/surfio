b17e5426e2e9a5a43c3a95ce255f7dc4ad6d78a1
tlochsta <81081702+tlochsta@users.noreply.github.com>
2024-08-09 20:52:57 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+
+import { ChemicalServer } from "chemicaljs";
+import express from "express";
+const chemical = new ChemicalServer();
+
+const port = process.env.PORT || 3278;
+
+chemical.use(express.static("public", {
+    index: "index.html",
+    extensions: ["html"]
+}));
+
+chemical.error((req, res) => {
+    res.status(404);
+    res.send("404 Error");
+});
+
+chemical.listen(port, () => {
+    console.log("Chemical example listening on port ${port}");
+});
